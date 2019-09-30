bbec670d371fe4ed06a49a42a2d10fa946a10564
VMORI0112 <victorfernan_moriganador@hotmail.com>
2019-09-30 21:00:28 +0000

    Commit-Sep-30-2019-1700

--- index.js
+++ index.js
@@ -9,28 +9,29 @@ function generate1() {                   // Randomly get card and type of card(S
   let colorCard="" ;
 
       let colorCardNumber = Math.floor(Math.random() * 2) + 1;
+      document.querySelector("#cardnumber1").style.color = colorCardNumber ;
       switch(colorCardNumber) {
         case 1:
-           colorCard="red"
+           document.querySelector("#cardnumber1").style.color = "#000000"
            break;
         case 2:
-            colorCard="black"
+           document.querySelector("#cardnumber1").style.color = "#ff0000"
            break;
       }
 
       let typeCardNumber = Math.floor(Math.random() * 4) + 1;
       switch(typeCardNumber) {
         case 1:
-           typeCard="spades"
+           typeCard="&spades;"
            break;
         case 2:
-            typeCard="diamonds"
+            typeCard="&diams;"
            break;
          case 3:
-            typeCard="hearts"
+            typeCard="&hearts;"
             break;
         case 4:
-           typeCard="clovers"
+           typeCard="&clubs;"
            break;
      }
 
@@ -52,7 +53,7 @@ function generate1() {                   // Randomly get card and type of card(S
        // default:
     }
 
-      document.querySelector("#cardnumber1").innerHTML = `${cardString}<p>${colorCard} ${typeCard}</p>`;
+      document.querySelector("#cardnumber1").innerHTML = `${cardString}<p> ${typeCard}</p>`;
       document.querySelector("#p1go").disabled = true;
       playerResult1=card ;
 
@@ -65,28 +66,29 @@ function generate2() {                   // Randomly get card and type of card(S
   let colorCard2="" ;
 
       let colorCardNumber2 = Math.floor(Math.random() * 2) + 1;
+      document.querySelector("#cardnumber2").style.color = colorCardNumber2 ;
       switch(colorCardNumber2) {
         case 1:
-           colorCard2="red"
+           document.querySelector("#cardnumber2").style.color = "#000000"
            break;
         case 2:
-            colorCard2="black"
+             document.querySelector("#cardnumber2").style.color = "#ff0000"
            break;
       }
 
       let typeCardNumber2 = Math.floor(Math.random() * 4) + 1;
       switch(typeCardNumber2) {
         case 1:
-           typeCard2="spades"
+           typeCard2="&spades;"
            break;
         case 2:
-            typeCard2="diamonds"
+            typeCard2="&diams;"
            break;
          case 3:
-            typeCard2="hearts"
+            typeCard2="&hearts;"
             break;
         case 4:
-           typeCard2="clovers"
+           typeCard2="&clubs;"
            break;
      }
 
@@ -109,7 +111,7 @@ function generate2() {                   // Randomly get card and type of card(S
        // default:
     }
 
-      document.querySelector("#cardnumber2").innerHTML = `${card2}<p>${colorCard2} ${typeCard2}</p>`;
+      document.querySelector("#cardnumber2").innerHTML = `${card2}<p>${typeCard2}</p>`;
       document.querySelector("#p2go").disabled = true;
       playerResult2=card2 ;
       console.log(card2)
